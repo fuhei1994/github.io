085bbd7db2710a7f90d3cd9f8bf055f04b9bfd92
fuhei1994 <131531269+fuhei1994@users.noreply.github.com>
2024-01-15 20:23:01 +0800

    Add files via upload

---
+++ db/article.php
@@ -0,0 +1,40 @@
+<?php
+return array (
+  'about' => 
+  array (
+    'id' => 'about',
+    'cid' => 'default',
+    'title' => '关于本站',
+    'intro' => '',
+    'img' => '',
+    'time' => 1645844114,
+    'top' => 0,
+    'private' => 0,
+    'comment' => 0,
+    'views' => 0,
+    'comments' => 0,
+    'tag' => 
+    array (
+      0 => '默认',
+    ),
+  ),
+  'message' => 
+  array (
+    'id' => 'message',
+    'cid' => 'default',
+    'title' => '留言板',
+    'intro' => '',
+    'img' => '',
+    'time' => 1645844114,
+    'top' => 0,
+    'private' => 0,
+    'comment' => 1,
+    'views' => 0,
+    'comments' => 0,
+    'tag' => 
+    array (
+      0 => '默认',
+    ),
+  ),
+);
+?>
